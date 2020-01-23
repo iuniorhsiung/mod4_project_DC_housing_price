7b5c5bea6f1ccc2a821c88c5b9578c49b92f0305
NateL <iuniorhsiung@gmail.com>
2020-01-23 09:23:13 -0500

    updated

--- python_folder/python file/Test_p_value.py
+++ python_folder/python file/Test_p_value.py
@@ -1,9 +1,15 @@
 # Create a function to detect the hypothesis testing results. 1: reject null, 0: fail to reject null
-def test_p_value(p = .05, name = 'ttest'):
+# Create a function to detect the hypothesis testing results. 1: reject null, 0: fail to reject null
+def test_p_value(p = .05, name = 'ttest', data = test_matrix):
     name_list = []
     for i in range(len(test_matrix)):
-        if test_matrix["p_" + name][i] < p:
+        if data["p_" + name][i] < p:
             name_list.append(1) 
         else:
             name_list.append(0)   
-    test_matrix['index_' + name] = name_list
+    data['index_' + name] = name_list
+
+
+
+
+
--- python_folder/python file/test_matrix_kpss_dw.py
+++ python_folder/python file/test_matrix_kpss_dw.py
@@ -1,24 +1,31 @@
-#statistic, p-value for adfuller
-p_val_adf = []
-
-# statistic and p-value for kpss
-p_val_kpss = []
-
-# statistic and p-value for acorr_breusch_godfrey 
-p_val_bg = []
-
-# statistic and p-value for durbin_watson
-p_val_dw = []
-
-for i in range(len(col_name)):
-    temp = adfuller(df[col_name[i]], regression = 'ct')
-    p_val_adf.append(temp[1])
+def hypo_test(col_name = col_name, data = df):
+    # p-value for adfuller
+    p_val_adf = []
     
-    temp = kpss(df[col_name[i]], regression = 'ct')
-    p_val_kpss.append(temp[1])
-    
-    temp = durbin_watson(df[col_name[i]])
-    p_val_dw.append(temp)
+    # p-value for kpss
+    p_val_kpss = []
 
-test_matrix = pd.DataFrame(list(zip(col_name, p_val_adf, p_val_kpss, p_val_dw)), 
-               columns =['Features','p_adftest', 'p_kpsstest','p_dwtest'])
+    # p-value for acorr_breusch_godfrey 
+    p_val_bg = []
+
+    # p-value for Ljung–Box Q test
+    p_val_lj = []
+
+    # statistic for durbin_watson
+    stat_dw = []
+    for i in range(len(col_name)):
+        temp = adfuller(data[col_name[i]], regression = 'ct')
+        p_val_adf.append(temp[1])
+    
+        temp = kpss(data[col_name[i]], regression = 'ct')
+        p_val_kpss.append(temp[1])
+    
+        temp = acorr_ljungbox(data[col_name[i]], lags = 10)
+        p_val_lj.append(temp[1][0])
+    
+        temp = durbin_watson(data[col_name[i]])
+        stat_dw.append(temp)
+        
+    test_matrix = pd.DataFrame(list(zip(col_name, p_val_adf, p_val_kpss, p_val_lj, stat_dw)), 
+               columns =['Features','p_adftest', 'p_kpsstest','p_ljtest', 'stat_dw'])
+    return test_matrix
